f0bf23d05c4e2547d87ca0c7e06f0badac1a9700
Henrik Olai Olsbø Håland <156893021+HenrikHaaland@users.noreply.github.com>
2025-03-27 10:44:27 +0100

    smal things

--- src/side-bar.tsx
+++ src/side-bar.tsx
@@ -1,24 +1,26 @@
 import './style/side-bar.css'
-import { FileSpreadsheet } from 'lucide-react';
+import { FileSpreadsheet, List, NotebookPen, CalendarDays, ScanEye, Goal, Archive} from 'lucide-react';
 
 export default function SideBar() {
     return (
-        <div className="SideBar">
-            <div className='SideBar-section'>
-                <h1>Lists and Sheets</h1>
-                <button>Draft Sheets</button>
-                <button>To Do Lists</button>
-                <button>Notes</button>
-            </div>
-            <div className='SideBar-section'>
-                <h1>Planner</h1>
-                <button>Calendar</button>
-                <button>Goals</button>
-            </div>
-            <div className='SideBar-section'>
-                <h1>Tools</h1>
-                <button>Daily Focus</button>
-                <button>Archive</button>
+        <div className="sideBar">
+            <div className='sideBar-content'>
+                <div className='sideBar-section'>
+                    <h1>Lists and Sheets</h1>
+                    <button> <FileSpreadsheet/> Draft Sheets</button>
+                    <button> <List/> To Do Lists</button>
+                    <button> <NotebookPen/> Notes</button>
+                </div>
+                <div className='sideBar-section'>
+                    <h1>Planner</h1>
+                    <button> <CalendarDays/> Calendar</button>
+                    <button> <Goal/> Goals</button>
+                </div>
+                <div className='sideBar-section'>
+                    <h1>Tools</h1>
+                    <button> <ScanEye/> Daily Focus</button>
+                    <button> <Archive/> Archive</button>
+                </div>
             </div>
             
         </div>
