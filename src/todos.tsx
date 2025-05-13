ce745d665ee8a59c99b74205fd14054c09e99834
Henrik Olai Olsbø Håland <156893021+HenrikHaaland@users.noreply.github.com>
2025-05-14 00:06:27 +0200

    added styling

--- src/todos.tsx
+++ src/todos.tsx
@@ -87,24 +87,42 @@ export default function Todos() {
     }, []);
 
     return (
-        <div>
-            <div className="flex flex-row items-baseline">
-                <h1 className="">GTodos</h1>
-                <h2 className="text-3xl">todo list 0.1</h2>
+        <div className="min-h-screen bg-white-gray">
+            <div className="flex flex-col items-start max-w-md max-auto">
+                <div className="flex flex-row items-baseline">
+                    <h1 className="">GTodos</h1>
+                    <h2 className="text-3xl">todo list 0.1</h2>
 
+                </div>
             </div>
             
-            <div>
+            <div className="flex flex-content-start flex-col pb-30 bg-white-gray">
                 {todos.map((todo, index) => (
-                    <div key={index} className="bg-red-500 p-4 m-2 rounded">
-                        <h2><input onChange={(e) => done(e, todo.id)} type="checkbox" checked={todo.finished_at?true:false} />{todo.content}</h2>
+                    <div key={index} className="p-4 m-2 rounded flex items-center">
+                        
+                        <div>
+                            <input onChange={(e) => done(e, todo.id)} type="checkbox" checked={todo.finished_at?true:false} />
+                        </div>
+                        <div className="pl-4">
+                            {todo.content}
+                        </div>
+                        <div>
+                            <button className="bg-violet text-white">slette todo temp</button>
+                        </div>
+
                         {/*<p>{todo.finished_at}</p>*/}
                     </div>
                 ))}
             </div>
 
-            <input onChange={e => setTodoInput(e.target.value)} value={todoInput} type="text" placeholder="write your todo"/>
-            <button onClick={createTodo} className="bg-violet text-white">Make new todo</button>
+            
+            <div className="w-[calc(100vw-0vw)] bg-white-gray fixed bottom-0 left-0 p-5 drop-shadow-xl/50">
+                <div className="flex flex-row items-center gap-15">
+                    <button onClick={createTodo} className="bg-violet text-white">Make new todo</button>
+                    <input className="border-violet border-2 p-0.5 rounded-xl" onChange={e => setTodoInput(e.target.value)} value={todoInput} type="text" placeholder="write your todo"/>
+                </div>
+            </div>
+            
         </div>
 
         
